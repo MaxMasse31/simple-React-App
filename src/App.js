eb491fced1517ee635d202c979782057c4296374
Max Masse <maxmasse199@gmail.com>
2023-10-25 15:45:10 -0400

    add style

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Cart from "./Components/Cart";
 import ShoppingList from "./Components/ShoppingList";
 import "./style/Banner.css";
 import logo from "./assets/leaf+1.png";
+import "./style/Layout.css"
 
 function App() {
-  return (
-    <div>
-      <Banner>
-        <img src={logo} alt="La maison jungle"  className='lmj-logo' />
-        <h1 className="lmj-title">La maison jungle</h1>
-      </Banner>
-      <Cart />
-      <ShoppingList /> 
-    </div>
-  );
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
+			{/* <Footer /> */}
+		</div>
+	)
 }
 
-export default App;
+export default App
